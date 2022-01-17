4a98c834e141b9076e042bd4e756daa53ccab378
Kaito-a-bit <arakai0128@gmail.com>
2022-01-17 09:44:06 +0900

    finished tutorial 1-3

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 荒井海斗 on 2022/01/15.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+       LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
